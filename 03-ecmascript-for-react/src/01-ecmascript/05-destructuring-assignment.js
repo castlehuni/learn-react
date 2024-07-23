505a788f27419e11dc9397e53053040e72fe8a6d
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-23 09:41:50 +0900

    7/23일 오전 수업내용 - 분해할당 - 화살표 함수 - default parameter - rest parameter

--- 03-ecmascript-for-react/src/01-ecmascript/05-destructuring-assignment.js
+++ 03-ecmascript-for-react/src/01-ecmascript/05-destructuring-assignment.js
@@ -85,20 +85,26 @@ function spreadRender() {
     return removeSpaceHTMLString(/* html */ `
       <table class="table">
         <caption class="sr-only">${data.caption}</caption>
-        ${data.rows.reduce(function (htmlString, item) {
+        ${data.rows.reduce(function (
+          htmlString,
+          item /* {headline, content} */
+        ) {
           // 🔶 구조 분해 할당 구문을 사용해 item 객체에서 항목을 분해 및 할당합니다.
           // 참고: https://mzl.la/3Jfrwpm
+
+          const { headline, content } = item;
 
           return (
             htmlString +
             /* html */ `
               <tr>
-                <th>${item.headline}</th>
-                <td>${numberWithComma(item.content)}원</td>
+                <th>${headline}</th>
+                <td>${numberWithComma(content)}원</td>
               </tr>
             `
           );
-        }, "")}
+        },
+        "")}
       </table>
     `);
   }
--- 03-ecmascript-for-react/src/01-ecmascript/08-rest-parameters.js
+++ 03-ecmascript-for-react/src/01-ecmascript/08-rest-parameters.js
@@ -2,16 +2,18 @@
 // rest parameters
 
 function _sum() {
-  const numbers = Array.from(arguments);
+  const numbers = Array.from(arguments); // 배열 or 유사배열 => 유사배열
   return numbers.reduce((result, number) => result + number, 0);
 }
 
 // 🔶 나머지 매개변수를 사용해 sum 함수 코드 로직을 다시 작성합니다.
 // 참고: https://mzl.la/43Ro9yp
-const sum = () => {};
+const sum = (...numbers) => numbers.reduce((total, number) => total + number);
 
-let result1_1 = _sum(2, 3, 9, 12, 105);
+let result1_1 = _sum(2, 3, 9, 12, 105); // arguments
 let result1_2 = sum(2, 3, 9, 12, 105);
+console.log(result1_1);
+console.log(result1_2);
 console.log(Object.is(result1_1, result1_2));
 
 let result2_1 = _sum(90, 418, -7);
