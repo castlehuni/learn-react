2461f26da6b4b7be6938f1d5f0946ad84ac682ab
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-26 11:09:51 +0900

    함수 컴포넌트로 NumberList 만들기

--- 05-react-component-with-props/src/main.js
+++ 05-react-component-with-props/src/main.js
@@ -5,7 +5,7 @@ import { createRoot } from "https://esm.sh/react-dom";
 import listData from "./data/list.js";
 
 // 컴포넌트 불러오기
-import NumberList from "./components/NumberList.class.js";
+import NumberList from "./components/NumberList.function.js";
 import ArchitectureList from "./components/architectures/ArchitectureList.class.js";
 import ArchitectureItem from "./components/architectures/ArchitectureItem.class.js";
 
@@ -23,8 +23,10 @@ if (container) {
     ),
   });
 
+  const numberList = React.createElement(NumberList, { count: 9 });
+
   // DOM 요소를 리액트 돔 루트로 만든 후, 리액트 앱 렌더링
-  createRoot(container).render(architectureList);
+  createRoot(container).render(numberList);
 }
 // 존재하지 않는다면?
 else {
