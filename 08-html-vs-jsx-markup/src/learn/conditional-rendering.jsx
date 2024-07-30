7317c436ee566325efcac6ef08073f177965b234
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-30 16:35:56 +0900

    prop 타입 검사 함수 마무리와 적용하기

--- 08-html-vs-jsx-markup/src/learn/conditional-rendering.jsx
+++ 08-html-vs-jsx-markup/src/learn/conditional-rendering.jsx
@@ -1,11 +1,18 @@
-import { isTrueOrFalse, typeOf } from '../utils';
+import { isTrueOrFalse } from '../utils';
+import PropTypes from '../utils/prop-types';
+
+console.log(PropTypes);
+
+// 이미지 경로 불러오기
+import kakaoTalkImagePath from '../assets/kakao-talk.svg?url';
+import nextJsImagePath from '../assets/next-js.svg?url';
 import reactImagePath from '../assets/react.svg?url';
 import viteImagePath from '../assets/vite.svg?url';
-import nextJsImagePath from '../assets/next-js.svg?url';
-import kakaoTalkImagePath from '../assets/kakao-talk.svg?url';
 
+// 이미지 타입 배열 관리
 const IMAGE_TYPES = ['react', 'vite', 'next.js', 'kakao talk'];
 
+// 컴포넌트
 function ConditionalRendering({ imageType }) {
   // 조건부 렌더링
   // 함수 몸체 (function body) 영역 안에서
@@ -13,6 +20,8 @@ function ConditionalRendering({ imageType }) {
   let imagePath = '';
   let printText = '';
 
+  // 조건 "문"
+  // if 문 => switch 문으로 변경
   switch (imageType.toLowerCase()) {
     case 'react':
       imagePath = reactImagePath;
@@ -34,36 +43,31 @@ function ConditionalRendering({ imageType }) {
       printText = '허용된 이미지 타입이 존재하지 않습니다.';
   }
 
-  // if (imageType.toLowerCase().includes('react')) {
-  //   imagePath = reactImagePath;
-  //   printText = 'React';
-  // }
+  // 값을 반환하는 함수 실행
+  // const isShowSpinner = isTrueOrFalse(); // 참 또는 거짓
 
-  // if (imageType.toLowerCase().includes('vite')) {
-  //   imagePath = viteImagePath;
-  //   printText = 'Vite';
-  // }
-
-  // if (imageType.toLowerCase().includes('next.js')) {
-  //   imagePath = nextJsImagePath;
-  //   printText = 'Next.js';
-  // }
-
-  // if (imageType.toLowerCase().includes('kakao talk')) {
-  //   imagePath = kakaoTalkImagePath;
-  //   printText = 'Kakao Talk';
-  // }
-
+  // 3항 연산자 (표현) "식"
   const spinnerOrVite = isTrueOrFalse() ? (
     <img className="spinner" src="/icons/spinner.svg" alt="로딩 중..." />
   ) : (
     <img src="/vite.svg" alt="Vite" style={{ height: 42 }} />
   );
 
+  // 논리 곱/합 연산자
+  // 그리고(AND, &&), 또는(OR, ||)
+  // const spinnerMessage = isShowSpinner ? '스피너 표시' : null;
+  // const spinnerMessage = isShowSpinner && '스피너 표시';
+  // const spinnerMessage = !isShowSpinner || '스피너 표시';
+
+  // JSX 반환 (마크업 생성)
+  // Q. JSX는 문(statement)이다? 아니, 표현식(expression)이다!
+  //    JSX 내부에서는 오직 식만 사용 가능하다!!!
   return (
     <>
+      {/* <dt>조건부 렌더링(conditional rendering) ({spinnerMessage})</dt> */}
       <dt>
-        조건부 렌더링(conditional rendering)({isTrueOrFalse() && '스피너 표시'})
+        조건부 렌더링(conditional rendering) ({isTrueOrFalse() && '스피너 표시'}
+        )
       </dt>
       <dd>
         <p>이미지 타입(image type)에 따라 렌더링 여부를 결정합니다.</p>
@@ -77,6 +81,7 @@ function ConditionalRendering({ imageType }) {
       <dd style={{ marginTop: 12 }}>
         <p>spinner 또는 vite 이미지가 랜덤으로 화면에 렌더링 되도록 합니다.</p>
         <div className="conditionalRendering">
+          {spinnerOrVite}
           {/* {randomNumber(0, 1) > 0.5 ? (
             <img
               className="spinner"
@@ -86,70 +91,20 @@ function ConditionalRendering({ imageType }) {
           ) : (
             <img src="/vite.svg" alt="Vite" style={{ height: 42 }} />
           )} */}
-          {spinnerOrVite}
         </div>
       </dd>
     </>
   );
 }
 
+// 컴포넌트 기본 내보내기
 export default ConditionalRendering;
 
+// 컴포넌트 속성(들) 검사
 ConditionalRendering.propTypes = {
-  imageType(props, propName, componentName) {
-    const propValue = props[propName];
-    const propType = typeOf(propValue);
-    const allowedType = 'string';
-    const typeMatchString = IMAGE_TYPES.reduce((result, type, index, array) => {
-      const divider = index < array.length - 1 ? '|' : '';
-      return result + type + divider;
-    }, '');
-
-    const typeMatch = new RegExp(`^(${typeMatchString})$`, 'i');
-
-    // 타입 검사
-    if (propType !== allowedType || !typeMatch.test(propValue)) {
-      // 타입이 다르면 오류 처리
-      throw new Error(
-        // `${componentName} 컴포넌트 ${propName} 속성 타입은 "${allowedType}" 타입이 요구되나, 실제 전달된 타입은 "${propType}"입니다.`
-        `${componentName} 컴포넌트 ${propName} 속성에 설정 가능한 값은 "[${IMAGE_TYPES.toString()}]" 중 하나가 요구되나, 실제 전달된 속성 값은 "${propValue}"입니다.`
-      );
-    }
-
-    // 그렇지 않으면 통과
-  },
+  // imageType 속성 검사
+  // 배열에 포함된 것 중 하나를 확인하는 검사
+  // ['react', 'vite', 'next.js', 'kakao talk']
+  // imageType: oneOf(['react', 'vite', 'next.js', 'kakao talk']),
+  imageType: PropTypes.oneOf(IMAGE_TYPES),
 };
-
-// {/* imageType 값이 'vite'인 경우 Vite 이미지를, 'react'인 경우 React 이미지를 화면에 표시합니다. */}
-// 삼항 연산자로 내가 작성한 부분
-// {imageType === 'react' ? (
-//   <img src="/src/assets/react.svg" alt="리액트 로고" />
-// ) : imageType === 'vite' ? (
-//   <img src="/src/assets/vite.svg" alt="비트 로고" />
-// ) : (
-//   <p>일치하는 사진이 없습니다</p>
-// )}
-// {/* imageType이 'vite'인 경우 'Vite', 'react'인 경우 'React' 텍스트를 화면에 표시합니다. */}
-// {imageType === 'react' ? (
-//   <p>React</p>
-// ) : imageType === 'vite' ? (
-//   <p>Vite</p>
-// ) : (
-//   <p>일치하는 텍스트가 없습니다</p>
-// )}
-
-{
-  // 내가 작성한 코드
-  /* <p>spinner 또는 vite 이미지가 랜덤으로 화면에 렌더링 되도록 합니다.</p>
-<div className="conditionalRendering">
-  {randomNumber(0, 1) === 0 ? (
-    <img
-      className="spinner"
-      src="/icons/spinner.svg"
-      alt="로딩 중..."
-    />
-  ) : (
-    <img src="/vite.svg" alt="Vite" style={{ height: 42 }} />
-  )}
-</div> */
-}
