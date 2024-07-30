3ccae8d8be2e584101eab536e9830d4f0601e45e
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-07-30 09:43:54 +0900

    파일 내용 변경 및 pnpm 사용

--- 08-html-vs-jsx-markup/src/learn/jsx-markup.jsx
+++ 08-html-vs-jsx-markup/src/learn/jsx-markup.jsx
@@ -2,6 +2,9 @@ import DataBinding from './data-binding';
 import ConditionalRendering from './conditional-rendering';
 import ConditionalDisplay from './conditional-display';
 import RenderLists from './render-lists';
+import * as learnData from '../data/learn';
+
+console.log(learnData);
 
 function JSX_Markup() {
   return (
