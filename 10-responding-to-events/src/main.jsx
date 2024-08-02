10171f10d77ed11dc16c5d22c72ab2a1c94ce21a
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-02 16:40:29 +0900

    리랜더링 할 수 있는 초기환경 추가(주석처리)

--- 10-responding-to-events/src/main.jsx
+++ 10-responding-to-events/src/main.jsx
@@ -22,3 +22,38 @@ if (container) {
 } else {
   console.warn('문서에 "#react-app" 요소가 존재하지 않습니다.');
 }
+
+/* -------------------------------------------------------------------------- */
+/*                               re-rendering 하기                            */
+/* -------------------------------------------------------------------------- */
+
+// import './styles/globals.css';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import Learn from './learn';
+
+// const container = document.getElementById('react-app');
+
+// if (!container) {
+//   throw new Error('문서에 "#react-app" 요소가 존재하지 않습니다.');
+// }
+
+// const reactDOMRoot = createRoot(container);
+
+// function render(){
+//   reactDOMRoot.render(
+//     <StrictMode>
+//       <Learn />
+//     </StrictMode>
+//   );
+// }
+
+// // Reactivity (Proxy)
+// export function update(){
+//   // update state
+//   // re-render
+//   render();
+// }
+
+// // first render
+// render();
