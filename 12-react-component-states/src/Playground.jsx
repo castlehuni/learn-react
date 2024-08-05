45556042c0aa79bf526185339993f7396bb073e7
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-05 11:41:47 +0900

    이용 약관 선언적으로 제어 Step 1

---
+++ 12-react-component-states/src/Playground.jsx
@@ -0,0 +1,10 @@
+import TermAndCondition from './components/TermAndCondition.class';
+
+function Playground() {
+  return (
+    <>
+      <TermAndCondition />
+    </>
+  );
+}
+export default Playground;
