27ab2d141fbb2813774b92942423233edf8ffcd0
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-06 11:24:58 +0900

    카운터 선언적 제어 III ->클래스 활용

--- 12-react-component-states/src/Playground.jsx
+++ 12-react-component-states/src/Playground.jsx
@@ -1,9 +1,11 @@
-import Counter from './components/Counter.class';
+import Counter from '@/components/Counter/Counter';
+import CounterClass from '@/components/Counter/Counter.class';
 
 function Playground() {
   return (
     <>
-      <Counter count={5} step={1} />
+      <Counter count={3} min={2} max={6} />
+      <CounterClass min={9} count={10} step={10} />
     </>
   );
 }
