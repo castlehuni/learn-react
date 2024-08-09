2df345719296b03b30407a24b4361f365a9a27b8
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-09 16:02:46 +0900

    틱택토 게임 만들기 Part 12. 학습 - 게임 상태 메시지 표시

--- 13-tic-tac-toe-game/src/tic-tac-toe/components/Status/Status.jsx
+++ 13-tic-tac-toe-game/src/tic-tac-toe/components/Status/Status.jsx
@@ -1,13 +1,29 @@
-import S from './Status.module.css';
+// --------------------------------------------------------------------------
+// ✅ 틱택토 게임 만들기 (Status 컴포넌트)
+// --------------------------------------------------------------------------
+// - [ ]
+// --------------------------------------------------------------------------
 
-function Status() {
-  return (
-    <>
-      <h2 className={S.component}>플레이어 : 🐶</h2>
-      {/* <h2>승자! : 🟨</h2> */}
-      {/* <h2>비겼어요... 😳</h2> */}
-    </>
-  );
+import { OneOfPlayerType } from '@/tic-tac-toe/types/type.d';
+import S from './Status.module.css';
+import { bool } from 'prop-types';
+
+Status.propTypes = {
+  winner: OneOfPlayerType,
+  nextPlayer: OneOfPlayerType.isRequired,
+  isDraw: bool,
+};
+
+function Status({ winner, nextPlayer, isDraw = false }) {
+  if (winner) {
+    return <h2 className={S.component}>위너!! {winner}</h2>;
+  }
+
+  if (isDraw) {
+    return <h2 className={S.component}>웁스... 비겼네... 😩 한 판 더?!</h2>;
+  }
+
+  return <h2 className={S.component}>다음 플레이어 : {nextPlayer}</h2>;
 }
 
 export default Status;
