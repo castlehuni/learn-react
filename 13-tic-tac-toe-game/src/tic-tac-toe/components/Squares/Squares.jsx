9448b52ba99db6e2c2eee1b1c4d73afd56293940
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-09 10:16:04 +0900

    square 컴포넌트 구현과 스타일링 연결 및 메인 스타일 연결

--- 13-tic-tac-toe-game/src/tic-tac-toe/components/Squares/Squares.jsx
+++ 13-tic-tac-toe-game/src/tic-tac-toe/components/Squares/Squares.jsx
@@ -1,11 +1,12 @@
+import { PLAYER } from '@/tic-tac-toe/constant';
 import Square from '../Square/Square';
 
 function Squares() {
   return (
     <div className="Squares">
-      <Square />
-      <Square />
-      <Square />
+      <Square></Square>
+      <Square>{PLAYER.ONE}</Square>
+      <Square>{PLAYER.TWO}</Square>
       <Square />
       <Square />
       <Square />
