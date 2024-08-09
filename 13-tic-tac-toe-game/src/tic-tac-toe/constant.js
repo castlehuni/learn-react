452355eec1203609d9b4c09bb876b550e562a374
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-09 11:00:57 +0900

    틱택토 게임 만들기 Part 5. - 게임 상태와 파생된 상태 - 이벤트 핸들러를 반환하는 함수 (클로저 활용)

--- 13-tic-tac-toe-game/src/tic-tac-toe/constant.js
+++ 13-tic-tac-toe-game/src/tic-tac-toe/constant.js
@@ -1,4 +1,11 @@
+// 게임에서 사용되는 말(플레이어)를 상수로 정의합니다.
+// const CONSTANT = VALUE
 export const PLAYER = {
   ONE: '🐶',
   TWO: '🐱',
 };
+
+export const PLAYER_COUNT = Object.keys(PLAYER).length;
+
+// 스퀘어 집합: 초기 상태 값
+export const INITIAL_SQUARES = Array(9).fill(null);
