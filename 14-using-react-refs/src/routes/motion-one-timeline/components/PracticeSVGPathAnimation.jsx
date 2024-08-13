320fa17da0c9579070ec974c2a988eb6e66e47c1
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-13 14:03:25 +0900

    타임라인 애니메이션 IV 학습 - Figma → SVG 패스 애니메이션 보완 - 하위 컴포넌트의 DOM 노드 노출 설정 - forwardRef() 활용

--- 14-using-react-refs/src/routes/motion-one-timeline/components/PracticeSVGPathAnimation.jsx
+++ 14-using-react-refs/src/routes/motion-one-timeline/components/PracticeSVGPathAnimation.jsx
@@ -10,84 +10,47 @@
 //   - [x] visibility
 //   - [x] pathLength
 // --------------------------------------------------------------------------
-import { string } from 'prop-types';
-import S from './PracticeSVGPathAnimation.module.css';
-import { useRef } from 'react';
 import { timeline } from 'motion';
+import { useRef } from 'react';
+import CircleLine from './CircleLine';
+import S from './PracticeSVGPathAnimation.module.css';
 
-PracticeSVGPathAnimation.propTypes = {
-  color: string,
-};
+function PracticeSVGPathAnimation() {
+  const svgRef = useRef(null);
 
-function PracticeSVGPathAnimation({ color = '#4729B4 ' }) {
-  const circle1Ref = useRef(null);
-  const barRef = useRef(null);
-  const circle2Ref = useRef(null);
+  const handleSVGPathAnimation = () => {
+    const { current: el } = svgRef;
 
-  const handleOnclick = () => {
-    const circle1Element = circle1Ref.current;
-    const barElement = barRef.current;
-    const circle2Element = circle2Ref.current;
+    const [circle1, circle2] = Array.from(el.querySelectorAll('circle'));
+    const line = el.querySelector('line');
 
-    const sequence = [
+    timeline(
       [
-        circle1Element,
-        { strokeDashoffset: [1, 0], visibility: 'visible' },
-        { duration: 1, easing: 'ease-out' },
+        [circle1, { strokeDashoffset: [1, 0], visibility: ['visible'] }],
+        [line, { strokeDashoffset: [1, 0], visibility: ['visible'] }],
+        [circle2, { strokeDashoffset: [1, 0], visibility: ['visible'] }],
       ],
-      [
-        barElement,
-        { strokeDashoffset: [1, 0], visibility: 'visible' },
-        { duration: 0.6, easing: 'ease-in-out', at: '-0.2' },
-      ],
-      [
-        circle2Element,
-        { strokeDashoffset: [1, 0], visibility: 'visible' },
-        { duration: 1, easing: 'ease-out' },
-      ],
-    ];
-
-    timeline(sequence);
+      {
+        duration: 1.6,
+        easing: 'cubic-bezier(0.79,0.14,0.15,0.86)',
+      }
+    );
   };
+
   return (
-    <div className={S.component} onClick={handleOnclick}>
-      <svg width={210} height={41} viewBox="0 0 210 41" fill="none">
-        <circle
-          cx="20.5"
-          cy="20.5"
-          r="17.5"
-          stroke={color}
-          strokeDasharray={1}
-          strokeDashoffset={0}
-          strokeWidth={6}
-          pathLength={1}
-          ref={circle1Ref}
-        />
-        <line
-          x1={35}
-          y1={20}
-          x2={173}
-          y2={20}
-          stroke={color}
-          strokeDasharray={1}
-          strokeDashoffset={0}
-          strokeWidth={6}
-          pathLength={1}
-          ref={barRef}
-        />
-        <circle
-          cx="189.5"
-          cy="20.5"
-          r="17.5"
-          stroke={color}
-          strokeDasharray={1}
-          strokeDashoffset={0}
-          strokeWidth={6}
-          pathLength={1}
-          ref={circle2Ref}
-        />
-      </svg>
-    </div>
+    <>
+      <button
+        type="button"
+        className={S.button}
+        onClick={handleSVGPathAnimation}
+      >
+        SVG 패스 애니메이션
+      </button>
+
+      <div className={S.component}>
+        <CircleLine ref={svgRef} />
+      </div>
+    </>
   );
 }
 
