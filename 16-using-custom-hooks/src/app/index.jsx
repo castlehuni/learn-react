771e6d3f298a0196d338d66a9b217431754d20d4
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-19 11:29:46 +0900

    16-using-custom-hooks 초기 환경 설정

---
+++ 16-using-custom-hooks/src/app/index.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const routes = [{}];
+
+const router = createBrowserRouter(routes);
+
+console.log(router);
+
+function App() {
+  return <></>;
+}
+
+export default App;
